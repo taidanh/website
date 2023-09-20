73d7abab34d6461c4b8e550d1f9c023807cfbd59
Tai Danh <taid100@gmail.com>
2023-09-20 15:33:19 -0700

    Updates footer to link to source code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <footer class="footer bg-dark text-white text-center py-3 fixed-bottom w-100">
-    &copy; 2023 Tai Danh. All rights reserved.
+    &copy; 2023 Tai Danh. All rights reserved. Website source code:
+    <a href="https://github.com/taidanh/website" target="_blank">
+      <b-icon-github class="mr-2"></b-icon-github>
+    </a>
   </footer>
 </template>
 
